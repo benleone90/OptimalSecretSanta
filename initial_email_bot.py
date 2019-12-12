32b03ce25cdaf5a8dd35931f1b6bb6cb51e5d951
whunt1965 <55954723+whunt1965@users.noreply.github.com>
2019-12-11 19:40:03 -0500

    Commenting Code
    We explored these two final options for email sending and chose the asynchronous one

--- initial_email_bot.py
+++ initial_email_bot.py
@@ -1,8 +1,8 @@
 #https://www.youtube.com/watch?v=QnDWIZuWYW0
-from flask import Flask, render_template, request, redirect, url_for#be sure to include last 2 for token generator
+from flask import Flask, render_template, request, redirect, url_for#include for url generator
 from flask_mail import Mail, Message
 #import smtplib#include
-from itsdangerous import URLSafeSerializer#include for url generator
+from itsdangerous import URLSafeSerializer#include for URL generator
 from threading import Thread#include for asynch emailing
 
 
@@ -17,7 +17,7 @@ app.config.update(#include
 	MAIL_PORT=465,
 	MAIL_USE_SSL=True,
 	MAIL_USERNAME = 'ec327emailb@gmail.com',
-	MAIL_PASSWORD = '#see me for this ,
+	MAIL_PASSWORD = #'#see me for this ,
     	SECRET_KEY = #see me for this,
     	MAIL_MAX_EMAILS = 1000
 	)
@@ -28,11 +28,11 @@ s = URLSafeSerializer(app.config['SECRET_KEY']) #include
 
 users = [{'GroupID': '1', 'UUID': '1', 'name': 'wiley', 'email': 'whunt@bu.edu'}, {'GroupID': '1', 'UUID': '2', 'name': 'wiley', 'email': 'wileyhunt65@gmail.com'}]
 
-def generate_token(email): #include
+def generate_token(email): #This function generates our email token
     token = s.dumps(email, salt= 'email-confirm')
     return token
 
-def send_async_email(msg):#send asynch email
+def send_async_email(msg):#This function sends asynchronous email
     with app.app_context():
         mail.send(msg)
 
@@ -49,7 +49,7 @@ def about():
     return 'Hi Ken'
  
 
-@app.route('/bulk/', methods = ['GET']) # bulk sending -- slow but reliable
+@app.route('/bulk/', methods = ['GET']) # The following is designed to send bulk emails. This holds the connection open (slow but reliable)
 def bulk():
     if request.method == 'GET':
         with mail.connect() as conn:
